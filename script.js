867cf654a9f16ab29a43d6f226c6bfc400f032f9
Sibasis Badatya <98338367+Sibasisbadatya@users.noreply.github.com>
2022-06-22 20:48:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function s1(){
+window.open("https://sibasisbadatya.github.io/appliacation-form.github.io/");
+}
+function s2(){
+  window.open("https://sibasisbadatya.github.io/bodymassindex.github.io/");  
+}
+function s3(){
+    window.open("")
+}
+function s4(){
+    window.open("https://sibasisbadatya.github.io/secretnumber.github.io/");  
+}
+function s5(){
+    window.open("https://sibasisbadatya.github.io/fastmath.github.io/"); 
+}
+function s6(){
+    window.open("https://sibasisbadatya.github.io/calculator.github.io/"); 
+}
+function s7(){
+    window.open("https://sibasisbadatya.github.io/digitalclock.github.io/");
+}
+function s8(){
+    window.open("https://sibasisbadatya.github.io/analogclock.github.io/");
+}
+function s9(){
+    window.open("https://sibasisbadatya.github.io/sunriseandset.github.io/");
+}
+function s10(){
+    window.open("https://sibasisbadatya.github.io/TicTacToe.github.io/");
+}
